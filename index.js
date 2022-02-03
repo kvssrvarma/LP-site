b6685e7d1eec6370a4f94a34416d851f833262e6
kvssrvarma <64119905+kvssrvarma@users.noreply.github.com>
2022-02-03 15:00:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ console.log("***visitorName****", visitorName);
 //var urlParams = new URLSearchParams(queryString);
 //var customerName = urlParams.get('CustomerName');
 //alert(customerName);
-userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
+//userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
+
+doNextCallback();
 
 //if(customerName != ""){
 document.getElementById("userDetails").innerHTML = userText;
@@ -38,12 +40,52 @@ lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
 pathtoDataConsumer = "visitorInfo.visitorName";
 
 
+var doNextCallback = function(){
+	
+
+///////////////////
+
+var updateCallbackConsumer = function(data) {
+console.log("***updateCallback****");
+console.log(JSON.stringify(data));
+var path = data.key;
+var value = data.newValue;
+consumerId = value;
+console.log("***consumerId****", consumerId);
 
 
 
+//var queryString = window.location.search;
+//console.log(queryString);
+//var urlParams = new URLSearchParams(queryString);
+//var customerName = urlParams.get('CustomerName');
+//alert(customerName);
 
 
+//if(customerName != ""){
+
+//}
+userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: "+consumerId+" </br>Your Payment Posting Date: PAYMENTDATE";
 document.getElementById("userDetails").innerHTML = userText;
+};
+
+
+ var notifyWhenDoneConsumer = function(err) {
+        if (err) {
+            // Do something with the error
+        }
+        // called when the bind is completed successfully,
+        // or when the action terminated with an error.
+};
+lpTag.agentSDK.bind(pathtoDataConsumer, updateCallbackConsumer, notifyWhenDoneConsumer);
+}
+//////////////////
+
+
+
+
+
+//document.getElementById("userDetails").innerHTML = userText;
 
 var readQuery = function(){
 	//let queryText = document.getElementById("userDetails").value;
