34ad79e8e6bc083caf86f9381d6caa5036ddcfa0
kvssrvarma <64119905+kvssrvarma@users.noreply.github.com>
2022-02-03 16:56:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ pathToData = "visitorInfo.visitorName";
 
 var userText;
 var visitorName;
-var consumerId;
+var visitorId;
 
 var updateCallback = function(data) {
 	console.log("***updateCallback****");
@@ -14,7 +14,7 @@ var updateCallback = function(data) {
 
 	//calling the second method to read the second variable
 	doNextCallback();
-	document.getElementById("userDetails").innerHTML = userText;
+	
 
 };
 //Can be used to handle error scenario
@@ -29,29 +29,30 @@ var updateCallback = function(data) {
 lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
 
 //path to the second variable
-pathtoDataConsumer = "visitorInfo.visitorId";
+pathtoDataVisitorId = "visitorInfo.visitorId";
 
 //Second callback function to retrieve second variable
 var doNextCallback = function(){
-	var updateCallbackConsumer = function(data) {
+	var updateCallbackVisitorId = function(data) {
 	console.log("***updateCallback****");
 	console.log(JSON.stringify(data));
 	var path = data.key;
 	var value = data.newValue;
-	consumerId = value;
-	console.log("***consumerId****", consumerId);
-	userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: "+consumerId+" </br>Your Payment Posting Date: PAYMENTDATE";
+	visitorId = value;
+	console.log("***visitorId****", visitorId);
+	userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: "+visitorId+"."
+	document.getElementById("userDetails").innerHTML = userText;
 };
 
 
- var notifyWhenDoneConsumer = function(err) {
+ var notifyWhenDoneVisitorId = function(err) {
         if (err) {
             // Do something with the error
         }
         // called when the bind is completed successfully,
         // or when the action terminated with an error.
 };
-lpTag.agentSDK.bind(pathtoDataConsumer, updateCallbackConsumer, notifyWhenDoneConsumer);
+lpTag.agentSDK.bind(pathtoDataVisitorId, updateCallbackVisitorId, notifyWhenDoneVisitorId);
 }
 //Function to copy text to chat window
 var readQuery = function(){
