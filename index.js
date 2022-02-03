2086b9f4bd17f90e6b9925731f36b3b9f551c732
kvssrvarma <64119905+kvssrvarma@users.noreply.github.com>
2022-02-03 14:02:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-pathToData = "chatTranscript.lines";
+pathToData = "visitorInfo.visitorName";
 
+let visitorName;
 var updateCallback = function(data) {
 console.log("***updateCallback****");
 console.log(JSON.stringify(data));
 var path = data.key;
 var value = data.newValue;
-var index = value.length - 1;
-let queryObj = value[index];
-let updatedQueryObjText = "";
-console.log("***index****", index);
-console.log("***index****", JSON.stringify(queryObj));
+visitorName = value;
+console.log("***visitorName****", visitorName);
 };
 
-var queryString = window.location.search;
-console.log(queryString);
-var urlParams = new URLSearchParams(queryString);
-var customerName = urlParams.get('CustomerName');
+//var queryString = window.location.search;
+//console.log(queryString);
+//var urlParams = new URLSearchParams(queryString);
+//var customerName = urlParams.get('CustomerName');
 //alert(customerName);
-let userText = "Please verify your Express Payment information is correct: </br> Your Name:" +customerName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
+let userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
 
 //if(customerName != ""){
 document.getElementById("userDetails").innerHTML = userText;
