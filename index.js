0a43f2edf46026fb6552b7290d49ac5290f3196f
kvssrvarma <64119905+kvssrvarma@users.noreply.github.com>
2022-02-03 14:51:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ console.log("***visitorName****", visitorName);
 //var urlParams = new URLSearchParams(queryString);
 //var customerName = urlParams.get('CustomerName');
 //alert(customerName);
-//userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
+userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: PAYMENT </br>Your Payment Posting Date: PAYMENTDATE";
 
 //if(customerName != ""){
-//document.getElementById("userDetails").innerHTML = userText;
+document.getElementById("userDetails").innerHTML = userText;
 //}
 };
 
@@ -35,43 +35,10 @@ console.log("***visitorName****", visitorName);
 };
 lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
 
-
-
 pathtoDataConsumer = "visitorInfo.visitorName";
 
 
-var updateCallbackConsumer = function(data) {
-console.log("***updateCallback****");
-console.log(JSON.stringify(data));
-var path = data.key;
-var value = data.newValue;
-consumerId = value;
-console.log("***consumerId****", consumerId);
 
-
-
-//var queryString = window.location.search;
-//console.log(queryString);
-//var urlParams = new URLSearchParams(queryString);
-//var customerName = urlParams.get('CustomerName');
-//alert(customerName);
-
-
-//if(customerName != ""){
-
-//}
-userText = "Please verify your Express Payment information is correct: </br> Your Name:" +visitorName+ " </br>Your Payment Amount: "+consumerId+" </br>Your Payment Posting Date: PAYMENTDATE";
-};
-
-
- var notifyWhenDoneConsumer = function(err) {
-        if (err) {
-            // Do something with the error
-        }
-        // called when the bind is completed successfully,
-        // or when the action terminated with an error.
-};
-lpTag.agentSDK.bind(pathtoDataConsumer, updateCallbackConsumer, notifyWhenDoneConsumer);
 
 
 
